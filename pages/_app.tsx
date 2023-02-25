fe3eb9d547552ac0796fb384664b3288326759e3
Jose Cuevas <josecueram@gmail.com>
2023-02-24 21:35:30 -0600

    Update `_app.tsx` - Add `next/font` - Extend `chakra` theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Roboto } from "@next/font/google";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import "@/styles/globals.css";
+
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
+
+const theme = extendTheme({
+  breakpoints: {
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px",
+    "2xl": "1400px",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     </ChakraProvider>
   );
 }
