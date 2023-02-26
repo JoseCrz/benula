ac4c566192866c7075df32a4a4114919d4e817b0
Jose Cuevas <josecueram@gmail.com>
2023-02-25 20:59:08 -0600

    Update `<Layout />` styles

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -10,7 +10,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header position="absolute" top={0} />
+      <Header />
       {children}
       <Footer />
     </>
