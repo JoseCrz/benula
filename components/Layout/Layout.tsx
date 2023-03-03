83d9c7148069c713904e7e74d86f450a6e790442
Jose Cuevas <josecueram@gmail.com>
2023-03-03 08:41:05 -0600

    Update `<Layout />` props

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,15 +2,16 @@ import Head from "next/head";
 import { Header, type HeaderProps, Footer } from "@/components";
 
 type LayoutProps = {
+  title: string;
   headerVariant?: HeaderProps["variant"];
   children: React.ReactNode;
 };
 
-export function Layout({ headerVariant, children }: LayoutProps) {
+export function Layout({ title, headerVariant, children }: LayoutProps) {
   return (
     <>
       <Head>
-        <title>Benúla</title>
+        <title>{title}</title>
         <meta name="description" content="Café bello, postres bellos" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
