dc2956345604c251a0b760971c1644d5247ccc07
Jose Cuevas <josecueram@gmail.com>
2023-03-05 10:22:01 -0600

    Add basic implementation of mobile menu for `/menu` page

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,13 +1,18 @@
 import type { MenuItem } from "@/types";
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import chunk from "lodash.chunk";
 import * as Tabs from "@radix-ui/react-tabs";
-import { Box, Button, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import * as Accordion from "@radix-ui/react-accordion";
+import { Box, Button, Icon, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import {
+  IoChevronDownCircleOutline,
+  IoChevronUpCircleOutline,
+} from "react-icons/io5";
 import { Layout, Section, Container } from "@/components";
 
 import { mocktailsData, brunchData, sandwichData, dessertsData } from "@/mocks";
 
-const tabs = {
+const categories = {
   cafe: "cafe",
   mocktails: "mocktails",
   brunch: "brunch",
@@ -16,8 +21,6 @@ const tabs = {
 };
 
 export default function Menu() {
-  const [currentValue, setCurrentValue] = useState(tabs.mocktails);
-
   return (
     <Layout title="Menú | Benúla">
       <Section py={18}>
@@ -28,41 +31,8 @@ export default function Menu() {
           <Text mt={3} fontSize="18px">
             descripción de todo el menú aquí
           </Text>
-          <Box mt={[12]}>
-            <Tabs.Root value={currentValue} onValueChange={setCurrentValue}>
-              <Tabs.List aria-label="menu">
-                {/* <Tabs.Trigger asChild value={tabs.cafe}>
-                  <Button>bebidas con café</Button>
-                </Tabs.Trigger> */}
-                <Flex alignItems="center">
-                  <TabButton value={tabs.mocktails} currentValue={currentValue}>
-                    mocktails
-                  </TabButton>
-                  <TabButton value={tabs.brunch} currentValue={currentValue}>
-                    brunch
-                  </TabButton>
-                  <TabButton
-                    value={tabs.sandwiches}
-                    currentValue={currentValue}
-                  >
-                    entre panes
-                  </TabButton>
-                  <TabButton value={tabs.desserts} currentValue={currentValue}>
-                    postres
-                  </TabButton>
-                </Flex>
-              </Tabs.List>
-              <Box
-                width="100%"
-                height="2px"
-                backgroundColor="rgba(0, 0, 0, 0.04)"
-              />
-              <TabPanel value={tabs.mocktails} menuItems={mocktailsData} />
-              <TabPanel value={tabs.brunch} menuItems={brunchData} />
-              <TabPanel value={tabs.sandwiches} menuItems={sandwichData} />
-              <TabPanel value={tabs.desserts} menuItems={dessertsData} />
-            </Tabs.Root>
-          </Box>
+          <DesktopMenu />
+          <MobileMenu />
         </Container>
       </Section>
     </Layout>
@@ -70,7 +40,41 @@ export default function Menu() {
 }
 
 // ==============================
-// Local Components
+// Desktop Menu
+function DesktopMenu() {
+  const [currentValue, setCurrentValue] = useState(categories.mocktails);
+
+  return (
+    <Box display={["none", "block"]} mt={[12]}>
+      <Tabs.Root value={currentValue} onValueChange={setCurrentValue}>
+        <Tabs.List aria-label="menu">
+          <Flex alignItems="center">
+            <TabButton value={categories.mocktails} currentValue={currentValue}>
+              mocktails
+            </TabButton>
+            <TabButton value={categories.brunch} currentValue={currentValue}>
+              brunch
+            </TabButton>
+            <TabButton
+              value={categories.sandwiches}
+              currentValue={currentValue}
+            >
+              entre panes
+            </TabButton>
+            <TabButton value={categories.desserts} currentValue={currentValue}>
+              postres
+            </TabButton>
+          </Flex>
+        </Tabs.List>
+        <Box width="100%" height="2px" backgroundColor="rgba(0, 0, 0, 0.04)" />
+        <TabPanel value={categories.mocktails} menuItems={mocktailsData} />
+        <TabPanel value={categories.brunch} menuItems={brunchData} />
+        <TabPanel value={categories.sandwiches} menuItems={sandwichData} />
+        <TabPanel value={categories.desserts} menuItems={dessertsData} />
+      </Tabs.Root>
+    </Box>
+  );
+}
 
 type TabsButtonProps = {
   currentValue: string;
@@ -166,3 +170,148 @@ function TabPanel({ value, menuItems }: TabPanelProps) {
     </Tabs.Content>
   );
 }
+
+// ==============================
+// Mobile Menu
+
+function MobileMenu() {
+  const [currentValue, setCurrentValue] = useState(categories.mocktails);
+  return (
+    <Box display={["block", "none"]} mt={16}>
+      <Accordion.Root
+        type="single"
+        value={currentValue}
+        onValueChange={setCurrentValue}
+        collapsible
+        asChild
+      >
+        <Grid rowGap="18px">
+          <Accordion.Item value={categories.mocktails}>
+            <AccordionTrigger
+              value={categories.mocktails}
+              currentValue={currentValue}
+            >
+              mocktails
+            </AccordionTrigger>
+            <AccordionContent menuItems={mocktailsData} />
+          </Accordion.Item>
+          <Accordion.Item value={categories.brunch}>
+            <AccordionTrigger
+              value={categories.brunch}
+              currentValue={currentValue}
+            >
+              brunch
+            </AccordionTrigger>
+            <AccordionContent menuItems={brunchData} />
+          </Accordion.Item>
+          <Accordion.Item value={categories.sandwiches}>
+            <AccordionTrigger
+              value={categories.sandwiches}
+              currentValue={currentValue}
+            >
+              entre panes
+            </AccordionTrigger>
+            <AccordionContent menuItems={sandwichData} />
+          </Accordion.Item>
+          <Accordion.Item value={categories.desserts}>
+            <AccordionTrigger
+              value={categories.desserts}
+              currentValue={currentValue}
+            >
+              brunch
+            </AccordionTrigger>
+            <AccordionContent menuItems={dessertsData} />
+          </Accordion.Item>
+        </Grid>
+      </Accordion.Root>
+    </Box>
+  );
+}
+
+type AccordionTriggerProps = {
+  value: string;
+  currentValue: string;
+} & Accordion.AccordionTriggerProps;
+
+const AccordionTrigger = forwardRef<HTMLButtonElement, AccordionTriggerProps>(
+  ({ value, currentValue, children, ...rest }, ref) => {
+    const isActive = value === currentValue;
+    return (
+      <Accordion.Header>
+        <Accordion.Trigger asChild ref={ref} {...rest}>
+          <Button
+            width="100%"
+            display="flex"
+            justifyContent="space-between"
+            alignContent="center"
+            fontSize="24px"
+            color="#721B1B"
+            backgroundColor="transparent"
+            height="auto"
+            px={3}
+            py={6}
+            borderRadius={0}
+            borderTopRightRadius="2px"
+            borderTopLeftRadius="2px"
+            border="1px solid #D7D7D7"
+          >
+            {children}
+            <Icon
+              as={
+                isActive ? IoChevronUpCircleOutline : IoChevronDownCircleOutline
+              }
+              color="#721B1B"
+              fontSize="24px"
+            />
+          </Button>
+        </Accordion.Trigger>
+      </Accordion.Header>
+    );
+  }
+);
+
+AccordionTrigger.displayName = "AccordionTrigger";
+
+type AccordionContentProps = {
+  menuItems: MenuItem[];
+} & Accordion.AccordionContentProps;
+
+const AccordionContent = forwardRef<HTMLDivElement, AccordionContentProps>(
+  ({ menuItems, ...rest }, ref) => {
+    return (
+      <Accordion.Content ref={ref} asChild {...rest}>
+        <Box border="1px solid #D7D7D7" borderTop="none" px={4} py={3}>
+          {menuItems.map((item) => (
+            <Box key={item.name} borderBottom="1px solid #E4E4E4" pt={2} pb={5}>
+              <Flex alignItems="baseline">
+                <Text fontSize="18px" mr={3}>
+                  ${item.basePrice}
+                </Text>
+                <Text fontWeight="bold" fontSize="20px">
+                  {item.name}
+                </Text>
+              </Flex>
+              <Text mt={6}>{item.abstract}</Text>
+              {item.options &&
+                item.options.map((option, index) => (
+                  <Text
+                    mt={1}
+                    key={`${item.name}-option-${index}`}
+                    fontSize="18px"
+                    color="#4D4D4D"
+                  >
+                    ${option.optionPrice}{" "}
+                    <Text as="span" fontSize="16px">
+                      {option.optionDescription}
+                    </Text>
+                  </Text>
+                ))}
+            </Box>
+          ))}
+        </Box>
+      </Accordion.Content>
+    );
+  }
+);
+
+AccordionContent.displayName = "AccordionContent";
