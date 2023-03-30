08571774aa6581f4114b3f0daf0b6bf8c170c1be
Jose Cuevas <josecueram@gmail.com>
2023-03-29 19:55:35 -0600

    Add more `Dessert` queries - Add `getAllDessertSlugs` - Add `getDessertBySlug`

--- sanity/queries/dessert/dessertQueries.ts
+++ sanity/queries/dessert/dessertQueries.ts
@@ -1,6 +1,5 @@
 import { groq } from "next-sanity";
 import { sanityClient } from "@/sanity/client";
-
 import { dessertType, type Dessert } from "@/sanity/schemas";
 
 const latestDessertsQuery = groq`
@@ -21,4 +20,24 @@ export async function getAllDeserts(): Promise<Dessert[]> {
   if (!sanityClient) return [];
 
   return (await sanityClient.fetch(getAllDessertsQuery)) || [];
+}
+
+const allDessertSlugsQuery = groq`
+  *[_type == "${dessertType.name}" && defined(slug.current)][].slug.current
+`;
+
+export async function getAllDessertSlugs(): Promise<string[]> {
+  if (!sanityClient) return [];
+
+  return (await sanityClient.fetch(allDessertSlugsQuery)) || [];
+}
+
+const getDessertBySlugQuery = groq`
+  *[_type == "${dessertType.name}" && slug.current == $slug][0]
+`;
+
+export async function getDessertBySlug(slug: string): Promise<Dessert> {
+  if (!sanityClient) return {} as any;
+
+  return await sanityClient.fetch(getDessertBySlugQuery, { slug });
 }
