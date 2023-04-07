b50cf5f1d89a4fa65fa36aad7f991d3ab1b53029
Jose Cuevas <josecueram@gmail.com>
2023-04-06 19:36:52 -0600

    Force vercel redeploy

--- sanity/data/sanityData.ts
+++ sanity/data/sanityData.ts
@@ -2,8 +2,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const SANITY_PROJECT_ID = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 
-export const SANITY_DATASET = isProduction
-  ? process.env.NEXT_PUBLIC_SANITY_DATASET!
-  : "development";
+export const SANITY_DATASET = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export const SANITY_API_VERSION = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
